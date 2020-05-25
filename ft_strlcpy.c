887fc55f463a5652b3043776153ae05f2902d64c
Kris <n-me.ru>
2020-05-25 23:59:40 +0300

    l

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,26 +6,35 @@
 /*   By: spowers <spowers@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/22 20:47:56 by spowers           #+#    #+#             */
-/*   Updated: 2020/05/22 20:53:36 by spowers          ###   ########.fr       */
+/*   Updated: 2020/05/25 23:58:59 by spowers          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strlcpy(char *dst, const char *src, size_t n)
+unsigned int	ft_strlcpy(char *dest, const char *src, unsigned int size)
 {
-	size_t	i;
+	unsigned int	count;
 
-	i = 0;
-	while (src[i] && i < n)
+	count = 0;
+	if (size == 0)
+		return (count);
+	while (size > 1 && *src)
 	{
-		dst[i] = src[i];
-		i++;
+		*dest = *src;
+		++dest;
+		++src;
+		--size;
+		++count;
 	}
-	while (i < n)
+	*dest = '\0';
+	while (*dest || *src)
 	{
-		dst[i] = '\0';
-		i++;
+		if (*src)
+		{
+			++src;
+			++count;
+		}
 	}
-	return (dst);
+	return (count);
 }
