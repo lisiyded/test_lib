3297cc515c91223203f03c8e8d3358bbbaeb57a5
Kris <n-me.ru>
2020-05-26 18:37:33 +0300

    str

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: spowers <spowers@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/22 21:09:21 by spowers           #+#    #+#             */
-/*   Updated: 2020/05/22 21:14:08 by spowers          ###   ########.fr       */
+/*   Updated: 2020/05/26 18:36:52 by spowers          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,20 @@
 
 char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 {
-	size_t i;
+	size_t	i;
+	size_t	j;
 
 	i = 0;
-	if (*s2 == '\0')
+	if (s2[0] == '\0')
 		return ((char *)s1);
-	else
+	while (s1[i] && i < n)
 	{
-		while (*s1 != '\0' && n)
-		{
-			if (*s1 == *s2)
-			{
-				while (*(s1 + i) == *(s2 + i) && n - i)
-				{
-					i++;
-					if (*(s2 + i) == '\0')
-						return ((char *)s1);
-				}
-			}
-			n--;
-			s1++;
-		}
+		j = 0;
+		while (s2[j] && s1[i + j] == s2[j] && i + j < n)
+			j++;
+		if (s2[j] == '\0')
+			return ((char *)&s1[i]);
+		i++;
 	}
 	return (NULL);
 }
