152bc4201434b207bb8cb421f683cb5bfd978b7f
Kris <n-me.ru>
2020-05-27 01:53:42 +0300

    funishversion

--- ft_substr.c
+++ ft_substr.c
@@ -14,16 +14,18 @@
 
 char				*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	unsigned int	i;
-	char			*str;
+	char	*str;
+	size_t	i;
 
 	i = 0;
+	if (!s)
+		return (NULL);
 	str = (char *)malloc(sizeof(char) * len + 1);
-	if (str == NULL)
+	if (new == NULL)
 		return (NULL);
 	while (i < len)
 	{
-		str[i] = s[start + i];
+		str[i] = s[i + start];
 		i++;
 	}
 	str[i] = '\0';
